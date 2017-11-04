9124a2cda91babd512d0ab67ecd6c25127c22229
dukemiller <dukemiller928@hotmail.com>
2017-11-03 18:25:36 -0700

    Remove underline on images

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
+              linkImagesToOriginal: false,
               maxWidth: 590,
             },
           },
